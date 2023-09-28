dd2f3744d0848bcfcf123e31ef47f8a7a6183b93
DAZA TOVAR JAIME <jdaza17@cuc.edu.co>
2023-09-28 11:04:09 -0500

    style: Format padding for base container

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,11 @@ class HomePage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
-                padding: const EdgeInsets.only(left: 16.0, top: 16.0, right: 16.0),
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  top: 16.0,
+                  right: 16.0,
+                ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
